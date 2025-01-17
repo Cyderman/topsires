8edfab63c236403f7dcaf20a91809a8e4347ca69
Cyderman <si.james1@gmail.com>
2025-01-17 21:18:13 +0000

    Add files via upload

--- sires.py
+++ sires.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Load data
-file_path = "top_sires.csv"  # Update with your file path if necessary
+file_path = "top_sires.csv" 
 data = pd.read_csv(file_path)
 
 # Streamlit App
@@ -12,12 +12,12 @@ st.title("Top Sires for Season 24")
 archetypes = data['archetype'].unique()
 selected_archetype = st.multiselect("Select Archetype(s):", options=archetypes, default=archetypes)
 
-min_foals = st.number_input("Minimum Number of Foals:", min_value=5, value=5, step=5)
+min_foals = st.number_input("Minimum Number of Foals:", min_value=0, value=1, step=1)
 
 search_name = st.text_input("Search by Horse Name:").strip().lower()
 
 # Filtered Data
-filtered_data = data[(data['archetype'].isin(selected_archetype)) & (data['number_of_foals'] >= min_foals)]
+filtered_data = data[(data['archetype'].isin(selected_archetype)) & (data['foals'] >= min_foals)]
 
 if search_name:
     filtered_data = filtered_data[filtered_data['horse_name'].str.lower().str.contains(search_name)]
